7242b193cd7a1da4e12dc3c735a189aba36697ca
pranav kotwal <pkotwal24@gmail.com>
2024-03-20 13:18:24 +0530

    add styling to the table

--- client/src/components/ReportsList.jsx
+++ client/src/components/ReportsList.jsx
@@ -51,24 +51,26 @@ const ReportsList = () => {
     return (
     <div>
       {reportsByDate.map(({ date, reports }) => (
-        <div key={date}>
-          <h2>{date}</h2>
-          <table>
-            <thead>
+        <div key={date} className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          {/* <h2>{date}</h2> */}
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th>Report Name</th>
-                <th>Value</th>
-                <th>Unit</th>
+                <th scope="col" className="px-6 py-3">Date</th>
+                <th scope="col" className="px-6 py-3">Report Name</th>
+                <th scope="col" className="px-6 py-3">Value</th>
+                <th scope="col" className="px-6 py-3">Unit</th>
               </tr>
             </thead>
             <tbody>
               {reports.map(report => (
                 report.reports.map(({ _id, name, value, unit }) => (
-                  <tr key={_id}>
+                  <tr key={_id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
 
-                    <td>{name}</td>
-                    <td>{value}</td>
-                    <td>{unit}</td>
+                    <td className='py-3 px-6 text-slate-400'>{date}</td>
+                    <td className='py-3 px-6 text-stone-400'>{name}</td>
+                    <td className='py-3 px-6 text-stone-400'>{value}</td>
+                    <td className='py-3 px-6 text-stone-400'>{unit}</td>
                   </tr>
                 ))
               ))}
