2166a327ac0cf1f15766de1a290b6118ff0ec7cb
pranav kotwal <pkotwal24@gmail.com>
2024-03-25 16:03:38 +0530

    Add ref values to chart

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const Chart = ({ data }) => {
+
+
+
+
+
+    console.log(data)
+    const flattenedData = data.reduce((acc, item) => {
+            item.reports.forEach(report => {
+                acc.push({
+                    date: new Date(item.date).toISOString().split('T')[0],
+                    value: report.value,
+                    unit: report.unit,
+                    refh:120,
+                    refl:80
+                });
+            });
+            return acc;
+  }, []);
+    console.log('flattenedData',flattenedData)
+
+    const sortedFlattened = flattenedData.sort((a,b)=>new Date(a.date)- new Date(b.date))
+
+
   return (
-    <LineChart width={800} height={400} data={data}>
+    <LineChart width={800} height={400} data={sortedFlattened} >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey={data.date} />
-      <YAxis dataKey={data.value}/>
+      <XAxis dataKey="date" />
+      <YAxis />
       <Tooltip />
       <Legend />
+      <Line type="monotone" dataKey="refh" stroke="#880000" fill="#FF000022"/>
+      <Line type="monotone" dataKey="refl" stroke="#880000" fill="#FF000022"/>
+
       <Line type="monotone" dataKey="value" stroke="#8884d8" />
     </LineChart>
   );
