70bc4955fc20112f95f01ec8c455ed74cf130047
pranav kotwal <pkotwal24@gmail.com>
2024-03-25 12:28:47 +0530

    make rows clickable

--- client/src/components/ReportsList.jsx
+++ client/src/components/ReportsList.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState,useContext } from 'react';
 import axios from 'axios';
 import { ReportsContext } from '../store/report-details';
-
+import trash from '../assets/trash-solid.svg'
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const ReportsList = () => {
     const [reportsByDate, setReportsByDate] = useState([]);
     const {reports} = useContext(ReportsContext)
+    const navigate = useNavigate()
 
     useEffect(()=>{
         const getReports = async ()=>{
@@ -45,40 +48,40 @@ const ReportsList = () => {
        
 
     },[reports])
+    
+    const handleRowClick=(row)=>{
+      navigate(`${row}`)
+
+    }
+
+    
+
 
     console.log('reportsByDate',reportsByDate)
     console.log('reports',reports)
     return (
-    <div>
+    <>
       {reportsByDate.map(({ date, reports }) => (
-        <div key={date} className="relative overflow-x-auto shadow-md sm:rounded-lg">
-          {/* <h2>{date}</h2> */}
-          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-3">Date</th>
-                <th scope="col" className="px-6 py-3">Report Name</th>
-                <th scope="col" className="px-6 py-3">Value</th>
-                <th scope="col" className="px-6 py-3">Unit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {reports.map(report => (
+              reports.map(report => (
                 report.reports.map(({ _id, name, value, unit }) => (
-                  <tr key={_id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
 
-                    <td className='py-3 px-6 text-slate-400'>{date}</td>
-                    <td className='py-3 px-6 text-stone-400'>{name}</td>
-                    <td className='py-3 px-6 text-stone-400'>{value}</td>
-                    <td className='py-3 px-6 text-stone-400'>{unit}</td>
+                  <tr key={_id} onClick={()=>handleRowClick(name)} className='hover:bg-red-400 hover:scale-102 cursor-pointer'  >
+                    
+                    <td className="border border-gray-300 px-4 py-2 text-center">{date}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{name}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{value}</td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">{unit}</td>
+                    <td className="border border-gray-300 px-4 py-2 flex justify-center ">
+                      <img src={trash} className='h-[30px]' alt="" />
+                    </td>
                   </tr>
+
+
                 ))
-              ))}
-            </tbody>
-          </table>
-        </div>
+              ))
+            
       ))}
-    </div>
+    </>
   );
 
 }
